61d400acbb90fa1995040d7817f1bade08bfce49
lemur <noctiphobic@gmail.com>
2022-08-06 13:53:13 -0500

    Create node class and initialization method

---
+++ node.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# node class for Binary Search Tree
+class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+
+  def initialize(data=nil, left=nil, right=nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
