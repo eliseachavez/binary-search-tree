d9825aadb0e849f4e1d50ee12a341e292dd58f29
lemur <noctiphobic@gmail.com>
2022-08-06 14:33:58 -0500

    Add pretty_print method provided by discord community

--- tree.rb
+++ tree.rb
@@ -54,5 +54,10 @@ class Tree
     # Returns the level 0 root node
   end
 
-  def
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
 end
